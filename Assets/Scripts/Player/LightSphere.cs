a83c1c8f07d1b96c002f40f372d0376bebbeb345
Niraculix <75939533+Niraculix@users.noreply.github.com>
2026-05-28 14:24:32 +0200

    Merge Resolve & Lightsphere changed

--- Assets/Scripts/Player/LightSphere.cs
+++ Assets/Scripts/Player/LightSphere.cs
@@ -1,25 +1,35 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class LightSphere : MonoBehaviour
 {
     public Vector2 dir;
     bool pickup_ready;
+    [SerializeField] private Transform GroundCheck;
+    [SerializeField] private LayerMask GroundLayers;
+    [SerializeField] private Rigidbody2D rb;
+
+    public float gravity;
+
+    public bool falling = true;
+
 
     public void Init(Vector2 dir, int dmg)
     {
         Vector2 force = new Vector2(0, Random.Range(10,50) * dmg);
         if(dir.x > 0)
         {
-            force.x = 200 * dmg;
+            force.x = 100 * dmg;
         }
 
         else
         {
-            force.x = -200 * dmg;
+            force.x = -100 * dmg;
         } 
 
-        GetComponent<Rigidbody2D>().AddForce(force);
+        rb.AddForce(force);
         StartCoroutine(pickupCooldown());
 
     }
@@ -28,6 +38,18 @@ public class LightSphere : MonoBehaviour
         if(pickup_ready) {
             collision.GetComponent<PlayerCombat>().pickupLight();
             Destroy(gameObject);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(GroundCheck.position, .1f, GroundLayers);
+
+        print(colliders[0]);
+
+        if(rb.linearVelocity.x != 0)
+        {
+            rb.linearVelocityX *= 0.99f;
         }
     }
 
